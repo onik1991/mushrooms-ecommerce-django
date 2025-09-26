274dbdb9fd7c448ac31fd8b259264dd17704d1c1
onik1991 <104652837+onik1991@users.noreply.github.com>
2025-09-26 02:03:47 +0100

    Add files via upload

---
+++ orders/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 
+                  'last_name', 
+                  'email', 
+                  'phone', 
+                  'address_line_1', 
+                  'address_line_2', 
+                  'city', 
+                  'county', 
+                  'postcode', 
+                  'country',
+                  'order_note'
+                  ]
